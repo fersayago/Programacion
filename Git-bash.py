7b55b3214186eeacf094912f129f8f56e73c53ad
Fer <sayago.fernando@gmail.com>
2019-10-14 14:19:28 -0300

    Trabajo
    Se agrego lo que me habia quedado en la PC de trabajo y una guia pequeña del git bash.

---
+++ Git-bash.py
@@ -0,0 +1,28 @@
+
+
+
+git status #nos muestra el estado actual del repo
+
+git stash #nos guarda los cambios en una pila "separada"
+
+#Clonnar proyecto
+
+git clone htpps://
+
+#para subir
+
+git add . #steagea los cambios 
+
+git commit -m  "Mensaje de que rompi" #empeaquet los cambios stageados
+
+git push origin develop  #sube cambios al repo remoto
+
+#Para bajar
+
+git checkout -b develop #crear un branch
+
+hit checkout develop #cambia al branch develop  
+
+git branch #chequear branches
+
+git pull origin develp #trae todo lo que se modifico en el branch
